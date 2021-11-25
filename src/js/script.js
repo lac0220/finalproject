7c9f02b6e21f5c392beb2ba4eeb271c18150a0f7
lac0220 <nemeth0220@gmail.com>
2021-11-25 09:23:10 +0100

    setting up git

---
+++ src/js/script.js
@@ -0,0 +1,69 @@
+
+// navigation
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+var modal = document.getElementById('id01');
+
+// clicking anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// home page sliding quotes
+var sliding = 0;
+showSlides(sliding);
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("Slide");
+  var dots = document.getElementsByClassName("Navdot");
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  sliding++;
+    if (sliding > slides.length) {sliding = 1}    
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" selected", "");
+  }
+  slides[sliding-1].style.display = "block";  
+  dots[sliding-1].className += " selected";
+  setTimeout(showSlides, 4000);
+}
+
+
+// home page sliding author pictures
+var slideIndex = 0;
+showSlides2();
+
+function showSlides2() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides_fade");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active_sliding_pictures", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active_sliding_pictures";
+  setTimeout(showSlides2, 4000); 
+}
+
+
+
+
+
