f6cc3c616f578ef45b80460c40bec50b866c3cb4
lac0220 <nemeth0220@gmail.com>
2022-09-07 02:40:35 +0200

    Add JS loader animation - Style navbar with fontawesome logos - Restructure JS - Reduce code repetition in SCSS

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,78 +5,33 @@ var __webpack_exports__ = {};
   !*** ./src/js/script.js ***!
   \**************************/
 
-// home page randome quotes
-const randomQuote = async () => {
-    const url ="https://type.fit/api/quotes";    
-    const response = await fetch(url);
-    console.log(typeof response);
-    const allQuotes = await response.json();
-  
-    const text = document.getElementById("quote");
-    const author = document.getElementById("author");
-  
-    //random number between 0 and the length of the quotes array
-    const indx = Math.floor(Math.random()*allQuotes.length);
-  
-    //storing at randomly generated index
-    let quote = allQuotes[indx].text;
-    let auth = allQuotes[indx].author;
-  
-    if (auth == null) {
-        auth = "Anonymous author";
-    }
-  
-    //displaying
-    text.innerHTML = "\"" + quote + "\"";
-    author.innerHTML = "- " + auth;
-}
-  
-  setInterval(randomQuote, 5000);
-  randomQuote();
+//loader-animation
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader-animation");
 
+    loader.classList.add("loader-animation-hidden");
 
-// home page sliding author pictures
-let slidingAuthor = 0;
-showAuthor();
+    loader.addEventListener("transform", () => {
+        document.body.removeChild("loader");
+    })
+}) 
 
-function showAuthor() {
-    let i;
-    let slides = document.getElementsByClassName("home-slides__img");
-    let dots = document.getElementsByClassName("home-dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slidingAuthor++;
-    if (slidingAuthor > slides.length) {slidingAuthor = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" sliding-pictures", "");
-    }
-    slides[slidingAuthor-1].style.display = "block";  
-    dots[slidingAuthor-1].className += " sliding-pictures";
-    setTimeout(showAuthor, 5000); 
-}
+ // hamburger menu open
+ const navHamburger = document.querySelector(".navbar-hamburger__btn");
+ const sideNav = document.querySelector("#side-nav");
 
-// home page sliding quotes
-let slidingQuotes = 0;
-showQuotes();
+ const mobileNavOpen = () => {
+     sideNav.classList.toggle("active");
+ }
+ navHamburger.addEventListener("click", mobileNavOpen);
 
-function showQuotes() {
-    let i;
-    let slides = document.getElementsByClassName("home-slides__quote");
-    let dots = document.getElementsByClassName("home-dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slidingQuotes++;
-        if (slidingQuotes > slides.length) {slidingQuotes = 1}    
-    for (i = 0; i < dots.length; i++) {
-        // set SCSS class
-        dots[i].className = dots[i].className.replace(" sliding-quotes", "");   
-    }
-    slides[slidingQuotes-1].style.display = "block";  
-    dots[slidingQuotes-1].className += " sliding-quotes";
-    setTimeout(showQuotes, 5000);
-}
+ // hamburger menu close
+ const navClose = document.querySelector(".navbar-close__btn");
+
+ const mobileNavClose = () => {
+     sideNav.classList.remove("active");
+ }
+ navClose.addEventListener("click", mobileNavClose);
 
 /******/ })()
 ;
